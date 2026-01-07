b1b4889dcbf6ccaf18c7b749cc5cb3b4b4fd5967
NimaiDev <nimaic.dev@gmail.com>
2026-01-07 21:17:24 +0530

    Refactor TestFsync function to include checksum calculation and update file path handling

--- learn/fsync_wall.go
+++ learn/fsync_wall.go
@@ -1,23 +1,40 @@
-package main
+package learn
 
 import (
 	"fmt"
+	"hash/crc32"
 	"os"
 )
 
 func TestFsync(iter int) {
-	file_name := "wal.log"
+	dir, _ := os.Getwd()
+	file_name := dir + "/wal.log"
 
 	file, err := os.OpenFile(file_name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
+
 	defer file.Close()
-	text := fmt.Sprintf("This is a content {%d}\n", iter)
+	text := fmt.Sprintf("This is aa content {%d}\n", iter)
+	checksum := fmt.Sprintf("Checksum: %d\n", CalculateCr32(text))
 	_, err = file.Write([]byte(text))
+	_, err = file.Write([]byte(checksum))
+
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
 	file.Sync()
+	fmt.Println("Written for Iter : ", iter)
+}
+
+const (
+	IEEE       = 0xedb88320
+	Castagnoli = 0x82f63b78
+	Koopman    = 0xeb31d82e
+)
+
+func CalculateCr32(data string) uint32 {
+	return crc32.Checksum([]byte(data), crc32.MakeTable(Koopman))
 }
--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/0x4E43/joker/learn"
 )
 
 func main() {
 	fmt.Println("Hello Joker")
 	i := 0
 	for {
-		TestFsync(i)
+		learn.TestFsync(i)
 		i++
 		time.Sleep(2 * time.Second)
 	}
