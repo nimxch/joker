73b1bd7d73029badada701799e8badcd640bf477
NimaiDev <nimaic.dev@gmail.com>
2026-01-12 23:32:14 +0530

    This is the proud piece of code I have ever written. pure wal implementation. pure systems

--- wal/wal_manager.go
+++ wal/wal_manager.go
@@ -1,14 +1,19 @@
 package wal
 
 import (
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"hash/crc32"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/nimxch/joker/custom"
 )
+
+var ErrEmptyPayload = errors.New("wal: empty payload")
 
 type WalManager struct {
 	fd *os.File
@@ -21,16 +26,6 @@ func InitWal(path string) (*WalManager, error) {
 	if strings.HasSuffix(path, string(os.PathSeparator)) {
 		return nil, custom.ErrInvalidFilePath
 	}
-	if strings.HasPrefix(path, "/") {
-		// sanitize string
-		path, _ = strings.CutPrefix(path, "/")
-	}
-	wd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-	}
-	path = fmt.Sprintf("%s/%s", wd, path)
-
 	// Extract the dir path and create it of not exist
 	dir := filepath.Dir(path)
 	if err := os.MkdirAll(dir, 0755); err != nil {
@@ -51,24 +46,57 @@ func InitWal(path string) (*WalManager, error) {
 	}, nil
 }
 
+//  Payload Structure
+// | recordLen (4) |
+// | crc32 (4)     |
+// | opType (1)    |
+// | payloadLen(4) |
+// | payload (N)   |
+
 func (w *WalManager) AppendEnqueue(payload []byte) error {
-	// 9 = 4byte[CRC] + 1byte[OptType]+ 4byte [PayloadLen]
-	recordLength := uint32(9) + uint32(len(payload))
-
-	walRecord := WalRecord{
-		payloadLen:   uint32(len(payload)),
-		payload:      payload,
-		opType:       uint8(OPERATION_TYPE_ENQUEUE),
-		crc32:        GetCrc(payload),
-		recordLength: recordLength,
+	// 9 = 4byte[CRC] + 1byte[OpType]+ 4byte [PayloadLen]
+	payloadLen := len(payload)
+	if payloadLen == 0 {
+		return ErrEmptyPayload
 	}
+	recordLength := uint32(4 + 1 + 4 + payloadLen)
 
-	err := walRecord.WriteFsync(w)
+	buf := make([]byte, 4+recordLength)
+
+	// Payload building starts
+	offset := 0
+	// Put 4 bytes of payload length to buffer
+	binary.LittleEndian.PutUint32(buf[offset:], recordLength) //recordLen
+	offset += 4
+	crcOffset := offset
+	offset += 4                          // CRC will be stored between crcOffset and offset
+	buf[offset] = OPERATION_TYPE_ENQUEUE // OpType
+	// Move offset to 1 byte, as len(opType is 1 byte)
+	offset += 1
+	binary.LittleEndian.PutUint32(buf[offset:], uint32(payloadLen)) // payload len
+	// Move offset to offset
+	offset += 4
+	copy(buf[offset:], payload)
+
+	crc := crc32.ChecksumIEEE(buf[crcOffset+4:]) // CRC after CRC field
+	//put crc offset into the buf
+	binary.LittleEndian.PutUint32(buf[crcOffset:], crc)
+	// payload building end
+	// Write to the wal and fsync
+	n, err := w.fd.Write(buf)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
-	fmt.Println(walRecord)
+	if n != len(buf) {
+		return io.ErrShortWrite
+	}
+
+	// do the fsync
+	if err := w.fd.Sync(); err != nil {
+		fmt.Println(err)
+		return err
+	}
 	return nil
 }
 
